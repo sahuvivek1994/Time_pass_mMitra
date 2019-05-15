e5f2a85afb5730e86d35f14f201534456849ec99
juilee26 <juilee.shinde@inscripts.in>
2019-05-15 18:31:03 +0530

    layouts in progess

--- app/src/main/java/tech/inscripts/ins_armman/mMitra/splash_screen/Splash_screen.kt
+++ app/src/main/java/tech/inscripts/ins_armman/mMitra/splash_screen/Splash_screen.kt
@@ -3,6 +3,7 @@ package tech.inscripts.ins_armman.mMitra
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import tech.inscripts.ins_armman.mMitra.login.Login
 import java.util.*
 import kotlin.concurrent.schedule
 
