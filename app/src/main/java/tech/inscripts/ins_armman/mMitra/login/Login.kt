8572b86d608616d6b66da847d71d8914f6f4919f
sahuvivek1994 <vivek.sahu@gmail.com>
2019-05-15 10:24:33 +0530

    complete with body structure

--- app/src/main/java/tech/inscripts/ins_armman/mMitra/login/Login.kt
+++ app/src/main/java/tech/inscripts/ins_armman/mMitra/login/Login.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.*
 import android.widget.EditText
 import tech.inscripts.ins_armman.mMitra.R
 import tech.inscripts.ins_armman.mMitra.mainMenu.MainActivity
@@ -33,7 +33,17 @@ class Login : AppCompatActivity() {
 
     fun login(view: View)
     {
+        val username = mEditTextUserName?.text.toString()
+        val password = mEditTextPassword?.text.toString()
+        Toast.makeText(this,"hello"+username,Toast.LENGTH_LONG).show()
+
+        hell()
+    }
+
+    fun hell()
+    {
         startActivity(Intent(this@Login, MainActivity::class.java))
+
     }
 
 }
